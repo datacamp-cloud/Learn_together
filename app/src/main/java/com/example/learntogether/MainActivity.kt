1532ac4ae0032bb8d7c81418357ede916ce86749
Datacamp-cloud <campbellsehebi04@gmail.com>
2024-09-25 22:22:14 +0000

    second commit for function setContent

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ShowInformation("JAMES", "first", "second")
+                    ShowInformation("JetPack Compose Tutorial", "first", "second")
                 }
             }
         }
